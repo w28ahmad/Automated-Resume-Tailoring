692c0ffcdc9ba430a14440bcaf4aef19b07c7583
w28ahmad <43382198+w28ahmad@users.noreply.github.com>
2019-04-29 01:46:24 -0400

    Creating a web server using express

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const hbs = require('hbs');
+const path = require('path');
+const express = require('express');
+
+// USING EXPRESS
+const app = express()
+const port = process.env.PORT || 3000
+
+// PATHS
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewspath = path.join(__dirname, '../templates/views')
+
+// Configering handlebars
+app.set('view engine', 'hbs');
+app.set('views', viewspath);
+
+
+// setting up static site
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'Automated Taloring',
+    })
+})
+
+app.listen(port, ()=>{
+    console.log('Server is up on port '+port);
+})
+
