85ff8f15ddd4a31c2a7559c0fcf1fe95d47c5ea4
w28ahmad <43382198+w28ahmad@users.noreply.github.com>
2019-04-29 18:11:04 -0400

    Now you are able to see you projects

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,53 @@ openResume = () =>{
     window.open("/Resume1", "__blank")
 }
 
-analyze = () =>{
-    const submit = document.getElementById("form")
-    submit.addEventListener('submit', (e)=>{
-        e.preventDefault()
-        var command = form.elements.command.value.toLowerCase();
-        if(command == "show resume"){
-            openResume();
-        }else if(command == "my projects"){
-            fetch('/projects').then((result)=>{
-                result.json().then((data)=>{
-                    showProjects(data);
-                })            
-            })
-        }
-        else{
-            console.log("I am not sure what you mean. Please type 'help' to show all options")
-        }
-    })
-}
+const submit = document.getElementById("form")
+submit.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    var command = form.elements.command.value.toLowerCase();
+    if(command == "show resume"){
+        openResume();
+    }else if(command == "my projects"){
+        fetch('/projects').then((result)=>{
+            result.json().then((data)=>{
+                showProjects(data.projects);
+            })            
+        })
+    }else if(command == "clear"){
+        clear();
+    }else if(command.includes("add project")){
+        console.log("true")
+    }
+    else{
+        console.log("I am not sure what you mean. Please type 'help' to show all options")
+    }
+})
 
 showProjects = (data) =>{
-    console.log(data);
+    projectDiv = document.getElementById("Project_div");
+
+    for(var i = 0; i < data.length; i++){
+        var project = document.createElement('h5');
+        setAttributes(project, {"style": "padding-bottom:5px;"});
+        project.textContent =i+ ".\t " +data[i];
+
+        projectDiv.appendChild(project)
+        projectDiv.appendChild(document.createElement('hr'))
+    }
+
+
+
+}
+
+function setAttributes(el, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
+
+clear = () =>{
+    var myNode = document.getElementById("Project_div");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
 }
